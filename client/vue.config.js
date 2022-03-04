671be73c1a1b5695f503d56fd24321253a256314
Aurelie Chicart <aurelie.chicart@gmail.com>
2022-03-04 20:10:46 +0100

    moving Vue front to client folder

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,6 +1,7 @@
 const {
   defineConfig
 } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -10,7 +11,7 @@ module.exports = defineConfig({
       // so this assumes you have a file named `src/variables.sass`
       // Note: this option is named as "prependData" in sass-loader v8
       sass: {
-        additionalData: `@import "~@/scss/variables.scss"`
+        additionalData: `@import "~@/client/scss/variables.scss"`
       },
       // by default the `sass` option will apply to both syntaxes
       // because `scss` syntax is also processed by sass-loader underlyingly
@@ -18,7 +19,7 @@ module.exports = defineConfig({
       // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
       // in that case, we can target the `scss` syntax separately using the `scss` option
       scss: {
-        additionalData: `@import "~@/scss/variables.scss";`
+        additionalData: `@import "~@/client/scss/variables.scss";`
       }
     }
   }
